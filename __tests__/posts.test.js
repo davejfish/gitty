de9c8472fbe394e7cf8532eca5cd1681df2d138e
Dave Fisher <dave.j.fish@gmail.com>
2022-08-11 10:36:34 -0700

    test written for insert

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Post = require('../lib/models/posts');
 
 jest.mock('../lib/services/github');
 const agent = request.agent(app);
@@ -21,6 +22,24 @@ describe('backend-express-template routes', () => {
       id: expect.any(String),
       userID: expect.any(String),
       content: expect.any(String),
+    });
+  });
+
+  it('#INSERT /posts should insert a post from a logged in user', async () => {
+    const user = await agent.get('/api/v1/github/callback?code=42').redirects(1);
+
+    const response = await agent.post('/api/v1/posts').send({
+      user_id: user.user_id,
+      content: 'lets go',
+    });
+
+    const posts = await Post.getAll();
+
+    expect(response.status).toBe(200);
+    expect(posts[posts.length - 1]).toEqual({
+      id: expect.any(String),
+      userID: '4',
+      content: 'lets go',
     });
   });
 
