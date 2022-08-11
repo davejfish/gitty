dba9b3aede703942c800bd7c9eacdcf0681a44db
Dave Fisher <dave.j.fish@gmail.com>
2022-08-10 18:31:39 -0700

    added test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,21 +10,9 @@ describe('backend-express-template routes', () => {
     return setup(pool);
   });
 
-  // it('should login and redirect users to /api/v1/github/dashboard', async () => {
-  //   const response = await request
-  //     .agent(app)
-  //     .get('/api/v1/github/callback?code=42')
-  //     .redirects(1);
-      
-  //   expect(response.body).toEqual({
-  //     id: expect.any(String),
-  //     username: 'fraud',
-  //     email: 'fraud@scam.com',
-  //     avatar: expect.any(String),
-  //     iat: expect.any(Number),
-  //     exp: expect.any(Number),
-  //   });
-  // });
+  it('test', () => {
+    expect(1).toEqual(1);
+  });
 
   afterAll(() => {
     pool.end();
