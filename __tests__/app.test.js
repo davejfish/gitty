b60e8f1dc0a8b442e275bbb7d422539e2aa0e6a9
Dave Fisher <dave.j.fish@gmail.com>
2022-08-10 17:23:27 -0700

    test written for mock test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,18 +1,48 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+jest.mock('../lib/services/github');
 
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
+// describe('backend-express-template routes', () => {
+//   beforeEach(() => {
+//     return setup(pool);
+//   });
 
-  afterAll(() => {
-    pool.end();
+//   // it('should login and redirect users to /api/v1/github/dashboard', async () => {
+//   //   const response = await request
+//   //     .agent(app)
+//   //     .get('/api/v1/github/callback?code=42')
+//   //     .redirects(1);
+    
+//   //   expect(response.body).toEqual({
+//   //     id: expect.any(String),
+//   //     username: 'fraud',
+//   //     email: 'fraud@scam.com',
+//   //     avatar: expect.any(String),
+//   //     iat: expect.any(Number),
+//   //     exp: expect.any(Number),
+//   //   });
+//   // });
+
+//   afterAll(() => {
+//     pool.end();
+//   });
+// });
+
+it('should login and redirect users to /api/v1/github/dashboard', async () => {
+  const response = await request
+    .agent(app)
+    .get('/api/v1/github/callback?code=42')
+    .redirects(1);
+    
+  expect(response.body).toEqual({
+    id: expect.any(String),
+    username: 'fraud',
+    email: 'fraud@scam.com',
+    avatar: expect.any(String),
+    iat: expect.any(Number),
+    exp: expect.any(Number),
   });
 });
